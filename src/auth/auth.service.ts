90704e75ca24da470fb118da0284c4c71e5ce98e
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 16:41:22 -0500

    refactor: add Prisma schema and connect to MongoDB in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, type OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class AuthService {
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(AuthService.name);
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Connected to MongoDB');
+  }
+
   register() {
     return 'register';
   }
