fef0803f225fb0921591d91a7e09739eeb134f67
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 19:44:48 -0500

    chore: update .env.template and envs.ts to include JWT_SECRET

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,6 +5,7 @@ interface EnvVars {
   PORT: number;
   NATS_SERVERS: string;
   DATABASE_URL: string;
+  JWT_SECRET: string;
 }
 
 const envsSchema = joi
@@ -12,6 +13,7 @@ const envsSchema = joi
     PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
     DATABASE_URL: joi.string().required(),
+    JWT_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -20,9 +22,7 @@ const { error, value } = envsSchema.validate({
   NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
 });
 
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
+if (error) throw new Error(`Config validation error: ${error.message}`);
 
 const envVars: EnvVars = value;
 
@@ -30,4 +30,5 @@ export const envs = {
   port: envVars.PORT,
   natsServers: envVars.NATS_SERVERS,
   databaseUrl: envVars.DATABASE_URL,
+  jwtSecret: envVars.JWT_SECRET,
 };
