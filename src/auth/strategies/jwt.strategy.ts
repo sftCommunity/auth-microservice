8d16859baaf22dab5bd5f4ae1167b3334807662b
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 03:37:35 -0500

    feat: Add JwtStrategy for JWT authentication

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { envs } from 'src/config';
+import { Repository } from 'typeorm';
+import { User } from '../entities';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {
+    super({
+      secretOrKey: envs.jwtSecret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { id } = payload;
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) throw new UnauthorizedException('Token not valid');
+    if (!user.is_active) throw new UnauthorizedException('User is not active');
+    return user;
+  }
+}
