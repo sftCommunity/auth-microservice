42f7a18d1945bbbaa2f6abe98eb7fb6845163281
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 03:39:08 -0500

    refactor: Update envs.ts to remove unused DATABASE_URL and add new environment variables for PostgreSQL configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,24 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   NATS_SERVERS: string;
-  DATABASE_URL: string;
   JWT_SECRET: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+  POSTGRES_DB: string;
+  PORT_DB: number;
+  HOST_DB: string;
 }
 
 const envsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
-    DATABASE_URL: joi.string().required(),
     JWT_SECRET: joi.string().required(),
+    POSTGRES_USER: joi.string().required(),
+    POSTGRES_PASSWORD: joi.string().required(),
+    POSTGRES_DB: joi.string().required(),
+    PORT_DB: joi.number().required(),
+    HOST_DB: joi.string().required(),
   })
   .unknown(true);
 
@@ -29,6 +37,10 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   natsServers: envVars.NATS_SERVERS,
-  databaseUrl: envVars.DATABASE_URL,
   jwtSecret: envVars.JWT_SECRET,
+  postgresUser: envVars.POSTGRES_USER,
+  postgresPassword: envVars.POSTGRES_PASSWORD,
+  postgresDb: envVars.POSTGRES_DB,
+  portDb: envVars.PORT_DB,
+  hostDb: envVars.HOST_DB,
 };
