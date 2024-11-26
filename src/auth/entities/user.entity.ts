a08d0cb3333f1a42a1f34c556242a15a2e71f9b9
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 03:37:17 -0500

    feat: Add User entity with necessary fields and validations

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,54 @@
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+    BeforeInsert,
+    BeforeUpdate,
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Column('text', { nullable: false })
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @Column('text', { unique: true, nullable: false })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Column('text', { nullable: false, select: false })
+  password: string;
+
+  @Column('bool', { default: true })
+  @IsBoolean()
+  is_active: boolean;
+
+  @Column('text', { array: true, default: ['user'] })
+  roles: string[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @BeforeInsert()
+  checkFieldBeforeInsert() {
+    this.email = this.email.toLowerCase();
+  }
+
+  @BeforeUpdate()
+  checkFieldBeforeUpdate() {
+    this.checkFieldBeforeInsert();
+  }
+}
