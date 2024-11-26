87a4f3e19c8495a197126ae69822cae0d8fc744a
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 03:36:02 -0500

    refactor: update app.module.ts to add TypeOrmModule for PostgreSQL database connection and import SeedModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST_DB,
+      port: +process.env.PORT_DB,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    SeedModule,
+  ],
 })
 export class AppModule {}
