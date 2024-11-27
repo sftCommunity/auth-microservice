6593a7c647a4eb135bd2f829eb761f2b99a7ea52
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 23:46:04 -0500

    feat: add RoleModule for managing roles

---
+++ src/role/role.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from './entities';
+import { RoleController } from './role.controller';
+import { RoleService } from './role.service';
+
+@Module({
+  controllers: [RoleController],
+  providers: [RoleService],
+  imports: [TypeOrmModule.forFeature([Role])],
+  exports: [RoleService],
+})
+export class RoleModule {}
