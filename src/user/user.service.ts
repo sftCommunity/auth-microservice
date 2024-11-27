46a2a51a9f0ca61384ffc7818cdb23c2a1936d44
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-27 04:25:57 -0500

    refactor: Update UserService to use UserPaginationDto for findAll method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
-import { PaginationDto } from 'src/common/dto';
+import { isEmail } from 'class-validator';
 import { Repository } from 'typeorm';
+import { validate as isUUID } from 'uuid';
+import { UserPaginationDto } from './dto';
 import { User } from './entities';
 
 @Injectable()
@@ -12,8 +14,8 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async findAll(paginationDto: PaginationDto) {
-    const { limit = 10, page = 1 } = paginationDto;
+  async findAll(userPaginationDto: UserPaginationDto) {
+    const { limit = 10, page = 1, is_active = true } = userPaginationDto;
     try {
       const total = await this.userRepository.count();
       return {
@@ -21,7 +23,7 @@ export class UserService {
           take: limit,
           skip: (page - 1) * limit,
           where: {
-            is_active: true,
+            is_active,
           },
         }),
         meta: {
@@ -30,6 +32,34 @@ export class UserService {
           total,
         },
       };
+    } catch (e) {
+      throw new RpcException(e);
+    }
+  }
+
+  async findOne(term: string) {
+    let user: User;
+    try {
+      if (isUUID(term)) {
+        user = await this.userRepository.findOne({
+          where: { id: term },
+        });
+      } else if (isEmail(term)) {
+        user = await this.userRepository.findOne({
+          where: { email: term },
+        });
+      } else {
+        const queryBuilder = this.userRepository.createQueryBuilder('user');
+        user = await queryBuilder
+          .where(`UPPER(name) = :name`, {
+            name: term.toUpperCase(),
+          })
+          .getOne();
+      }
+
+      if (!user) throw new RpcException(`User with term ${term} not found`);
+
+      return user;
     } catch (e) {
       throw new RpcException(e);
     }
