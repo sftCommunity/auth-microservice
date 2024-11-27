557e16ec3787d4382b74c368df8a0106879022d9
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 23:55:44 -0500

    refactor: Update imports in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,34 +3,22 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { envs } from 'src/config';
+import { Role } from 'src/role/entities';
+import { User } from 'src/user/entities';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { LoginAttempt, PasswordReset, Role, Session, User } from './entities';
 import { JwtStrategy } from './strategies';
 @Module({
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   imports: [
-    TypeOrmModule.forFeature([
-      User,
-      Session,
-      LoginAttempt,
-      PasswordReset,
-      Role,
-    ]),
+    TypeOrmModule.forFeature([User, Role]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: envs.jwtSecret,
       signOptions: { expiresIn: '15d' },
     }),
   ],
-  exports: [
-    TypeOrmModule,
-    JwtStrategy,
-    PassportModule,
-    JwtModule,
-    AuthModule,
-    AuthService,
-  ],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule, AuthModule],
 })
 export class AuthModule {}
