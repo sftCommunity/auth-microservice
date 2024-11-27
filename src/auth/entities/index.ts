cd11796c807e272facef62f5472fe26f198afaaa
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 23:41:51 -0500

    refactor: remove unnecessary entity files

--- src/auth/entities/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './login_attempt.entity';
-export * from './password_reset.entity';
-export * from './role.entity';
-export * from './session.entity';
-export * from './user.entity';
-
--- src/auth/interfaces/valid-roles.ts
+++
@@ -1,5 +0,0 @@
-export enum ValidRoles {
-  ADMIN = 'admin',
-  SUPER_ADMIN = 'super_admin',
-  USER = 'user',
-}
