9765022f10969bc4f15e4232249dbb1c5dd43b3e
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 23:46:23 -0500

    feat: add RoleController for managing roles

---
+++ src/role/role.controller.ts
@@ -0,0 +1,35 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common/dto';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { RoleService } from './role.service';
+
+@Controller()
+export class RoleController {
+  constructor(private readonly roleService: RoleService) {}
+
+  @MessagePattern('role.create')
+  create(@Payload() createRoleDto: CreateRoleDto) {
+    return this.roleService.create(createRoleDto);
+  }
+
+  @MessagePattern('role.find.one')
+  findOne(@Payload() term: string) {
+    return this.roleService.findOne(term);
+  }
+
+  @MessagePattern('role.find.all')
+  findAll(@Payload() paginationDto: PaginationDto) {
+    return this.roleService.findAll(paginationDto);
+  }
+
+  @MessagePattern('role.delete.one')
+  delete(@Payload() term: string) {
+    return this.roleService.delete(term);
+  }
+
+  @MessagePattern('role.delete.all')
+  deleteAll() {
+    return this.roleService.deleteAllRoles();
+  }
+}
