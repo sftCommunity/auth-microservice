66e203af1c6bc917eac93fd7fd2df7347a1d2f28
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-26 23:36:36 -0500

    feat: Add ResetPasswordDto class for handling password reset in API requests

---
+++ src/auth/dto/reset-password.dto.ts
@@ -0,0 +1,17 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword
+} from 'class-validator';
+
+export class ResetPasswordDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
+  old_password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
+  new_password: string;
+}
