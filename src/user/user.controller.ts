227fdc8f87739098f8a75d97ae33d024734eca49
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-27 04:25:48 -0500

    refactor: Update UserController to use UserPaginationDto for findAll method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { PaginationDto } from 'src/common/dto';
+import { UserPaginationDto } from './dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -8,7 +8,12 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @MessagePattern('user.find.all')
-  findAll(@Payload() paginationDto: PaginationDto) {
-    return this.userService.findAll(paginationDto);
+  findAll(@Payload() userPaginationDto: UserPaginationDto) {
+    return this.userService.findAll(userPaginationDto);
+  }
+
+  @MessagePattern('user.find.one')
+  findOne(@Payload() term: string) {
+    return this.userService.findOne(term);
   }
 }
