d3d4ffd19c6bc01c1ca3553e051ec3ada71beec7
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-27 23:01:19 -0500

    feat: Add CreateSessionDto for creating session entities

---
+++ src/session/dto/create-session.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateSessionDto {
+  @IsUUID()
+  @IsString()
+  user_id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  token: string;
+
+  @IsString()
+  @IsNotEmpty()
+  user_agent: string;
+
+  @IsString()
+  @IsNotEmpty()
+  ip_address: string;
+
+  @IsOptional()
+  expires_at: Date;
+
+  @IsBoolean()
+  @IsOptional()
+  is_active: boolean = true;
+}
